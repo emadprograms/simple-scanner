1893172006afa4b79c4e4d3d61f56d844961546f
emadprograms <79793120+emadprograms@users.noreply.github.com>
2024-11-15 21:27:55 +0300

    added a grpah function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import streamlit as st
+import plotly.express as px
 
 # Define the ticker symbol
 ticker_symbol = 'AAPL'
@@ -12,3 +13,7 @@ ticker_df = ticker_data.history(period='1d', start='2020-1-1', end='2021-1-1')
 
 # Display the data using Streamlit
 st.write(ticker_df)
+
+# Plot the data using Plotly
+fig = px.line(ticker_df, x=ticker_df.index, y='Close', title='AAPL Stock Price')
+st.plotly_chart(fig)
